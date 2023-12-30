733a7bf61e3de2064efba13be041f78194c17e77
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2023-12-30 17:03:35 +0530

    update

---
+++ Arrays/src/com/jai/Input.java
@@ -0,0 +1,45 @@
+package com.jai;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Array of primitives
+        int[] arr = new int[5];
+        arr[0] = 5;
+        arr[1] = 34;
+        arr[2] = 45;
+        arr[3] = 323;
+        arr[4] = 643563;
+        // {5, 34, 45, 323, 643563}
+        System.out.println(arr[3]);
+
+        //input using for loops
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = in.nextInt();
+//        }
+//
+//        System.out.println(Arrays.toString(arr));
+//        for (int j : arr) { // for every element in array it print element
+//            System.out.print(j + " ");// j represents the element of array
+//        }
+
+//        System.out.println(arr[5]); index out of bound err
+
+
+
+        // Array of Objects
+        String[] Str = new String[5];
+        for (int i = 0; i < Str.length; i++) {
+            Str[i] = in.next();
+        }
+
+        System.out.println(Arrays.toString(Str));
+        // modify
+        Str[2] = "Jai"; // modify elements  by assigning ref var
+        System.out.println(Arrays.toString(Str));
+
+    }
+}
--- Arrays/src/com/jai/Main.java
+++ Arrays/src/com/jai/Main.java
@@ -8,9 +8,16 @@ public class Main {
         //syntax
         // datatype[] var_name = new datatype[size];
         //store 5 rno:
-        int[] rnos = new int[5];
-        //or directly
-        int[] rnos1 = {2,3,4,6,9};
+//
+        int[] res; //declaring array. res getting defined in stack
+        res = new int[5];// initializeation: object is being created in heap memory
+
+//        System.out.println(res[1]);// while assigning arr value it internally takes it as [0,0,0,0,0]
+
+//        String[] arr = new String[4];
+//        System.out.println(arr[0]);
+
+
 
 
     }
