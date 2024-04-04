9f1d2fbd17624e33268b5a66459b57114ed6f7ab
Jaiharan <136414923+Jaiharan@users.noreply.github.com>
2024-04-04 16:43:26 +0530

    update

--- LinkedList/src/com/jai/LL.java
+++ LinkedList/src/com/jai/LL.java
@@ -116,6 +116,33 @@ public class LL {
         return null;
     }
 
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (fast != slow);
+
+        if(slow == 1) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private int findSquare(int number) {
+        int ans = 0;
+        while(number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+
     private class Node {
         private int value;
         private Node next;
--- LinkedList/src/com/jai/Main.java
+++ LinkedList/src/com/jai/Main.java
@@ -5,6 +5,32 @@ import java.util.LinkedList;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (fast != slow);
+
+        if(slow == 1) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private static int findSquare(int number) {
+        int ans = 0;
+        while(number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         LL list = new LL();
         list.insertF(2343);
@@ -21,5 +47,7 @@ public class Main {
         list.display();
         System.out.println(list.delete(2));
         list.display();
+
+        System.out.println(isHappy(19));
     }
 }
